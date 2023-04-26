27a242560b5bdc72018f891f481102385f0507a0
“mikalojuszrg” <mikalojus.zirgulis@gmail.com>
2023-04-26 15:14:27 +0300

    added fictional data

---
+++ data/posts.ts
@@ -0,0 +1,30 @@
+export const posts = [
+  {
+    id: 1,
+    title: "Test 1",
+    description: "Test 1 Test 1 Test 1",
+    imageUrl:
+      "https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 2,
+    title: "Test 2",
+    description: "Test 2 Test 2 Test 2",
+    imageUrl:
+      "https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bmF0dXJlfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 3,
+    title: "Test 3",
+    description: "Test 3 Test 3 Test 3",
+    imageUrl:
+      "https://images.unsplash.com/photo-1465146344425-f00d5f5c8f07?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fG5hdHVyZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 4,
+    title: "Test 4",
+    description: "Test 4 Test 4 Test 4",
+    imageUrl:
+      "https://images.unsplash.com/photo-1510784722466-f2aa9c52fff6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fG5hdHVyZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60",
+  },
+];
